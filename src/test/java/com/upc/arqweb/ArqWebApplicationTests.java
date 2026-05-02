36bb4330af1ad9ad2db227c2b7096579adb290a9
SofiaAllca <u20171F413@upc.edu.pe>
2026-05-01 20:51:10 -0500

    Initial commit

---
+++ src/test/java/com/upc/arqweb/ArqWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.arqweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
